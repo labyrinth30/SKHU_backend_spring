d9a239a3bd175b287eb907887c9d2db5cd00bc39
younha00 <ayeon0@office.skhu.ac.kr>
2023-10-06 18:29:09 +0900

    10월 4일 강의
    Pagination

---
+++ mybatis7/src/main/java/net/skhu/service/StudentService.java
@@ -0,0 +1,75 @@
+package net.skhu.service;
+
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import net.skhu.dto.Student;
+import net.skhu.mapper.StudentMapper;
+import net.skhu.model.Pagination;
+import net.skhu.model.StudentEdit;
+
+@Service
+public class StudentService {
+
+    @Autowired StudentMapper studentMapper;
+    ModelMapper modelMapper = new ModelMapper();
+
+    public StudentEdit findOne(int id) {
+        Student studentDto = studentMapper.findOne(id);
+        return toEditModel(studentDto);
+    }
+
+    public Student findByStudentNo(String studentNo) {
+        return studentMapper.findByStudentNo(studentNo);
+    }
+
+    public List<Student> findAll(Pagination pagination) {
+        pagination.setRecordCount(studentMapper.getCount(pagination));
+        return studentMapper.findAll(pagination);
+    }
+
+    public void insert(StudentEdit studentEdit, BindingResult bindingResult,
+            Pagination pagination) throws Exception {
+        if (hasErrors(studentEdit, bindingResult))
+            throw new Exception("입력 데이터 오류");
+        Student student = toDto(studentEdit);
+        studentMapper.insert(student);
+        int lastPage = (int)Math.ceil((double)studentMapper.getCount(pagination) / pagination.getSz());
+        pagination.setPg(lastPage);
+    }
+
+    public void update(StudentEdit studentEdit,
+            BindingResult bindingResult) throws Exception {
+        if (hasErrors(studentEdit, bindingResult))
+            throw new Exception("입력 데이터 오류");
+        Student student = toDto(studentEdit);
+        studentMapper.update(student);
+    }
+
+    public void delete(int id) {
+        studentMapper.delete(id);
+    }
+
+    public Student toDto(StudentEdit studentEdit) {
+        return modelMapper.map(studentEdit, Student.class);
+    }
+
+    public StudentEdit toEditModel(Student studentDto) {
+        return modelMapper.map(studentDto, StudentEdit.class);
+    }
+
+    public boolean hasErrors(StudentEdit studentEdit, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) return true;
+        Student student2 = findByStudentNo(studentEdit.getStudentNo());
+        if (student2 != null && student2.getId() != studentEdit.getId()) {
+            bindingResult.rejectValue("studentNo", null, "학번이 중복됩니다.");
+            return true;
+        }
+        return false;
+    }
+}
+
