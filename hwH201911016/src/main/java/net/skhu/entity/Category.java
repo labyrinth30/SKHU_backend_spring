55fbb7cc65ab1118d89e5ba147f893eb2c66ddc0
younha00 <ayeon0@office.skhu.ac.kr>
2023-12-09 11:24:54 +0900

    12월 9일 시험 대비(Book2)

--- hwH201911016/src/main/java/net/skhu/entity/Category.java
+++ hwH201911016/src/main/java/net/skhu/entity/Category.java
@@ -2,14 +2,13 @@ package net.skhu.entity;
 
 import java.util.List;
 
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -17,24 +16,16 @@ import lombok.ToString;
 
 @Data
 @Entity
-public class Lecture {
+public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
 
-    String title;
-    int year;
-    String room;
-    String semester;
-
-    @ManyToOne
-    @JoinColumn(name = "professorId")
-    Professor professor;
+    String name;
 
     @JsonIgnore
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
-    @OneToMany(mappedBy="lecture")
-    List<Sugang> sugangs;
+    @OneToMany(mappedBy="category")
+    List<Book> books;
 }
-
---
+++ hwH201911016/src/main/java/net/skhu/model/BookEdit.java
@@ -0,0 +1,34 @@
+package net.skhu.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class BookEdit {
+    int id;
+
+    @NotEmpty @NotBlank
+    @Size(min=8, max=12)
+    String title;
+
+    @NotEmpty @NotBlank
+    @Size(min=2, max=20)
+    String author;
+
+    @NotEmpty @NotBlank
+    @Size(min=2, max=20)
+    String price;
+
+    @NotEmpty @NotBlank
+    @Size(min=2, max=20)
+    String publisher;
+
+    @Min(value=1, message="카테고리를 선택하세요.")
+    int categoryId;
+}
+
